eee7fa52fca90a9d45d77b9a9b5f59c725014db2
kunal <singalkunal10@gmail.com>
2021-09-03 21:14:47 +0530

    rename app

--- db/db.js
+++ db/db.js
@@ -6,10 +6,10 @@ async function dbConnect(){
   if(!process.env.MONGO_PASS) {
     throw new CustomError('Must provide MONGO_PASS env variable');
   }
-  
+
     try{
       await mongoose
-      .connect(`mongodb+srv://kunal:${process.env.MONGO_PASS}@cluster0.dl6ab.mongodb.net/ekart?retryWrites=true&w=majority`, { 
+      .connect(`mongodb+srv://kunal:${process.env.MONGO_PASS}@cluster0.dl6ab.mongodb.net/etark?retryWrites=true&w=majority`, { 
         useNewUrlParser: true,
         // useFindAndModify: false,
         // useCreateIndex: true,
